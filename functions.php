e476d94054625f43375db22855451d312d82061c
Jeffrey Pisa <jeffrey.pisa@gmail.com>
2022-04-03 17:56:47 +0000

    quote bg fix + remove cpt

--- functions.php
+++ functions.php
@@ -2,8 +2,8 @@
 
 include 'lib/menu.php';
 include 'lib/acf.php';
-include 'lib/cpt.php';
-include 'lib/ctax.php';
+// include 'lib/cpt.php';
+// include 'lib/ctax.php';
 include 'lib/gutenberg.php';
 include 'lib/includes.php';
 include 'lib/optionspage.php';
